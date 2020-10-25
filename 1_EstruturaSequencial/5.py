87f2a51ed7f02db1159f2c0b95b058c461705103
hig0d <61846967+hig0d@users.noreply.github.com>
2020-10-25 19:37:35 -0300

    Delete 5.py

--- 1_EstruturaSequencial/5.py
+++
@@ -1,7 +0,0 @@
-"""5. Faça um Programa que converta metros para centímetros."""
-
-try:
-    m = float(input('Digite uma quantidade em metros: '))
-    print(f'{m} metros em cm são: {m * 100}')
-except ValueError:
-    print('Por favor, digite um valor inteiro ou real!')
