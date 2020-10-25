19fca3860cc8f647298e2db742f0245148857873
hig0d <61846967+hig0d@users.noreply.github.com>
2020-10-25 19:37:12 -0300

    Delete 6.py

--- 1_EstruturaSequencial/6.py
+++
@@ -1,6 +0,0 @@
-"""6. Faça um Programa que peça o raio de um círculo, calcule e mostre sua área."""
-
-from math import pi
-
-raio = float(input('Digite a o raio do círculo: '))
-print(f'A área do círculo é de: {pi * raio ** 2:.2f}')
